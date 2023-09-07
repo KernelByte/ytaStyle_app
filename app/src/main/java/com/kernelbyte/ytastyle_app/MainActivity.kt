e4020686f3f3c70341a7f5d37b121f38eddec9e3
Yoniher Melendez <yonihermelendez@gmail.com>
2023-09-07 00:21:49 -0500

    Se termina parte de la logica de login de manera satisfactoria, falta SharedPreferences

--- app/src/main/java/com/kernelbyte/ytastyle_app/MainActivity.kt
+++ app/src/main/java/com/kernelbyte/ytastyle_app/MainActivity.kt
@@ -1,53 +1,107 @@
 package com.kernelbyte.ytastyle_app
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
-import android.os.Build
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.kernelbyte.ytastyle_app.databinding.ActivityMainBinding
+import com.kernelbyte.ytastyle_app.models.LoginResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-
-@SuppressLint("StaticFieldLeak")
-private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-//private val apiService : ApiService by lazy {
-//    ApiService.create()
-//}
+    private lateinit var itemBinding: ActivityMainBinding
+    private lateinit var  userPreferences: SharedPreferences
+    private var token: String? = null
+
+private val apiService : ApiService by lazy {
+    ApiService.create()
+}
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         // Poner mismo color parte de arriba de la app
         val w = window
         w.setFlags(
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         )
-
         supportActionBar?.hide()
+        itemBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(itemBinding.root)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        //val preferences = getSharedPreferences("jwt",Context.MODE_PRIVATE)
+            //token = userPreferences.getString("jwt", "")
 
-        //if (preferences.contains("."))
-        //    goToMenu()
+        itemBinding.btIniciar.setOnClickListener{
+            performLogin()
+        }
+
+
+        if (token?.contains(".") == true) {
+                goToMenu()
+        }
     }
 
-   // private fun goToMenu(){
-   //     val i = Intent(this,MenuActivity::class.java)
-   //     startActivity(i)
-   // }
 
-    /*private fun createSessionPreference(){
-        val preferences = PreferenceHelper.defaultPrefs(this)
-        preferences
-    }*/
 
+
+    // Metodo para iniciar sesion en el aplicativo
+    private fun performLogin(){
+        val et_email = itemBinding.etMail.text.toString()
+        val et_pass = itemBinding.etPass.text.toString()
+
+        val call = apiService.postLogin(et_email,et_pass)
+
+        call.enqueue(object :  Callback<LoginResponse>{
+            override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
+                if(response.isSuccessful){
+                    val loginResponse = response.body()
+                    if (loginResponse == null){
+                        Toast.makeText(applicationContext,"Usuario o contraseña invalidos",Toast.LENGTH_SHORT).show()
+                        return
+                    }
+
+                    // Si la comunicacion es correcta
+                    if (loginResponse.success){
+                        createSessionPreference(loginResponse.jwt)
+                        goToMenu()
+                        return
+                    }else{
+                        Toast.makeText(applicationContext,"Usuario o contraseña invalidos",Toast.LENGTH_SHORT).show()
+                    }
+                }  else{
+                    Toast.makeText(applicationContext,"Se produjo un error en el servidor",Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                Toast.makeText(applicationContext,"Se produjo un error en el servidor",Toast.LENGTH_SHORT).show()
+            }
+
+        })
+
+    }
+
+    // Metodo para crear y almacenar token en preferences
+    private fun createSessionPreference(jwt : String){
+        userPreferences = getSharedPreferences("login", Context.MODE_PRIVATE)
+        var editor = userPreferences.edit()
+        editor.putString("jwt",jwt)
+        editor.commit()
+    }
+
+
+    private fun goToMenu(){
+        val i = Intent(this,MenuActivity::class.java)
+        startActivity(i)
+        finish()
+    }
 }
--- app/src/main/java/com/kernelbyte/ytastyle_app/models/LoginResponse.kt
+++ app/src/main/java/com/kernelbyte/ytastyle_app/models/LoginResponse.kt
@@ -3,7 +3,7 @@ package com.kernelbyte.ytastyle_app.models
 import com.kernelbyte.ytastyle_app.models.Users
 
 data class LoginResponse(
-    val success: String,
+    val success: Boolean,
     val user: Users,
     val jwt: String
 )
